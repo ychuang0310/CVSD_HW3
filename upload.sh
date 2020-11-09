8f89bb294a1705ea849451c74357b32a4bba6ce3
nk930439gj94 <nk930439gj94@gmail.com>
2020-11-09 15:27:54 +0800

    upload sdc

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 scp 00_TESTBED/*.v iclab:CVSD_HW/CVSD_HW3/00_TESTBED
 scp 01_RTL/*.v iclab:CVSD_HW/CVSD_HW3/01_RTL
+scp 02_SYN/ipdc_dc.sdc iclab:CVSD_HW/CVSD_HW3/02_SYN
 scp 03_GATE/*.v iclab:CVSD_HW/CVSD_HW3/03_GATE
